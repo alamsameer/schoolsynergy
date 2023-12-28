e4d71b4600670ee665cc8cffa7e44c8639465063
Sameer Alam <alamsameer738@gmail.com>
2023-12-28 12:52:45 +0530

    added auth to the students

---
+++ src/controller/auth/student.js
@@ -0,0 +1,61 @@
+import bcrypt from 'bcrypt';
+import Student from '../../model/student/student';
+import jwt from "jsonwebtoken"
+
+export const OrganisationSignUp=async (req, res) => {
+    try {
+      const { name, email, password, studentclass} = req.body;
+      console.log({ name, email, password,studentclass} );
+      // Check if the user already exists
+      const existingUser = await Student.findOne({ email });
+      if (existingUser) {
+        return res.status(409).json({ error: 'Student already exists' });
+      }
+  
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Create a new user object
+      const newUser = {
+        name,
+        email,
+        password: hashedPassword,
+      };
+  
+      // Add the user to the databaseJJ
+      const newTeacherData = await Student.create(newUser);
+      const role="teacher"
+      // Generate a JWT token
+      const token = jwt.sign({ email,role}, process.env.JWT_SECRET, { expiresIn: '1h' });
+      // Return the token to the client
+      res.status(201).json({ message: 'Student registered successfully',token });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Error registering user' });
+    }
+  }
+
+export const OrganisationSignIn=async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log({  email, password} );
+    const user = await Student.findOne({ email });
+    console.log(user);
+    if (!user) {
+      return res.status(404).json({ error: 'Student not found' });
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(401).json({ error: 'Incorrect password' });
+    }
+    const role="organisation"
+    // Generate a JWT token
+    const token = jwt.sign({ email,role}, process.env.JWT_SECRET, { expiresIn: '1h' }); 
+    // Return the token to the client
+    res.json({ message: 'Student logged in successfully',token ,user });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: 'Error logging in user' });
+  }
+}
